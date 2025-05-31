ad05d7ba31e2637fa87c2871bf12965fe00737cc
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2025-05-31 18:50:15 +0900

    feat : add sign up, sigin api in auth.controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Auth } from './auth.type';
 
 @Controller('/api/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  public async signUp(@Body() request: Auth.SignUpRequest) {
+    return this.authService.signUp(request);
+  }
+
+  @Post('sign-in')
+  public async signIn(@Body() request: Auth.SignInRequest) {
+    return this.authService.signIn(request);
+  }
 }
