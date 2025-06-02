cd4a817d7c3ae252ee7f714ab2dc2914c5cd2e85
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2025-06-03 02:46:51 +0900

    feat: implement todo controller from spec

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,9 +1,48 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
-import { UserGuard } from 'src/common/guards/user.guard';
+import { User } from '../../common/decorators/user.decorator';
+import { CreateTodoDto, UpdateTodoDto } from './todo.dto';
+import { UserGuard } from '../../common/guards/user.guard';
 
 @UseGuards(UserGuard)
-@Controller('/api/v1/todo')
+@Controller('/api/v1/todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getTodos(@User() user: { id: string; email: string }) {
+    return await this.todoService.getTodos(user);
+  }
+
+  @Post()
+  async createTodo(
+    @User() user: { id: string; email: string },
+    @Body() request: CreateTodoDto,
+  ) {
+    return await this.todoService.createTodo(user, request);
+  }
+
+  @Get(':id')
+  async getTodo(@Param('id') id: string) {
+    return await this.todoService.getTodo(id);
+  }
+
+  @Put(':id')
+  async updateTodo(@Param('id') id: string, @Body() request: UpdateTodoDto) {
+    return await this.todoService.updateTodo(id, request);
+  }
+
+  @Delete(':id')
+  async deleteTodo(@Param('id') id: string) {
+    return await this.todoService.deleteTodo(id);
+  }
 }
